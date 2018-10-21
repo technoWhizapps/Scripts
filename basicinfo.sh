b775316b2111175f97f369975bbdb673b0315c6f
technoWhizapps <s3671056@student.rmit.edu.au>
2018-10-21 19:31:40 +1100

    Initial draft of the Script for Part B of assignment 2 just has comments and the shell we are using at this stage

---
+++ basicinfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This script will take in a caommadn flag and from that setting send details about the system back to the user.
+# script followed by free - will show the available and memory consumption on the disk
+# script followed by Disk will show the abvailable and used diskspace on the system
+# script name followed by Net - will show the network connections on the system
+#script name followed by Up will show the uptime since last rebott for the system.
